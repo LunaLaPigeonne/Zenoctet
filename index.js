b9a788d2891dc19145ca9cffd310ec0114741749
Luna <yanngillot0@gmail.com>
2024-09-21 12:34:03 +0200

    Zenoctet v1.1.5

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const { ActivityType, Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
 client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
