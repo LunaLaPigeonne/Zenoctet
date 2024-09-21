12f2e3c6eb0345bbeac71110c402e2d5504b6dfb
Luna <yanngillot0@gmail.com>
2024-09-21 14:47:27 +0200

    Add files via upload

--- commandHandler.js
+++ commandHandler.js
@@ -1,16 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { Collection, MessageEmbed } = require('discord.js');
+require('dotenv').config();
 
 module.exports = async (client) => {
     client.commands = new Collection();
+    const commands = [];
     const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 
     for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
         const commandName = command.data.name;
-        client.commands.set(command.name, command);
+        client.commands.set(commandName, command);
+        commands.push(command.data.toJSON());
         console.log(`Command '${commandName}' loaded`);
+    }
+
+    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
     }
 
     client.on('interactionCreate', async interaction => {
@@ -23,7 +43,7 @@ module.exports = async (client) => {
         console.log(`Command '${interaction.commandName}' requested by ${interaction.user.tag}`);
 
         // Vérification des permissions
-        if (command.ownerOnly && interaction.user.id !== process.env.OWNER_ID) {
+        if (command.ownerOnly && interaction.user.id !== process.env.BOT_OWNER_ID) {
             return interaction.reply({ content: 'Cette commande est réservée au développeur du bot.', ephemeral: true });
         }
 
