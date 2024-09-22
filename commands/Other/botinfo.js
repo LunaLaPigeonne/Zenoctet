f3e4f71afcbee61dd7d0b814ddfa40e0ab61d10a
Luna <yanngillot0@gmail.com>
2024-09-22 16:09:25 +0200

    Add files via upload

--- commands/Other/botinfo.js
+++ commands/Other/botinfo.js
@@ -11,18 +11,34 @@ module.exports = {
     async execute(interaction) {
         const { client } = interaction;
 
+        // Calculer l'uptime en millisecondes
+        const uptime = client.uptime;
+        const uptimeSeconds = Math.floor(uptime / 1000);
+        const uptimeMinutes = Math.floor(uptimeSeconds / 60);
+        const uptimeHours = Math.floor(uptimeMinutes / 60);
+        const uptimeDays = Math.floor(uptimeHours / 24);
+
+        // Formater l'uptime en fonction de la durée
+        let uptimeString;
+        if (uptimeMinutes < 60) {
+            uptimeString = `${uptimeMinutes} minutes, ${uptimeSeconds % 60} secondes`;
+        } else if (uptimeHours < 24) {
+            uptimeString = `${uptimeHours} heures, ${uptimeMinutes % 60} minutes, ${uptimeSeconds % 60} secondes`;
+        } else {
+            uptimeString = `${uptimeDays} jours, ${uptimeHours % 24} heures, ${uptimeMinutes % 60} minutes, ${uptimeSeconds % 60} secondes`;
+        }
+
         const embed = new EmbedBuilder()
             .setColor('Blue')
             .setTitle('Informations sur le bot')
             .addFields(
                 { name: '<:nodejs:1287093728971849798> Version de Node.js', value: process.version },
-                { name: '<:discordjs:1287094503785758912> Version de Discord.js', value: require('discord.js').version},
+                { name: '<:discordjs:1287094503785758912> Version de Discord.js', value: require('discord.js').version },
                 { name: '<:linux:1287093584029552754> OS', value: os.platform() },
                 { name: '<:github:1287093218365673603> Hébergé sur', value: 'GitHub' },
                 { name: '<:heroku:1287093558855335977> Déployé sur', value: 'Heroku' },
-                { name: 'Uptime', value: `${Math.floor(client.uptime / 1000 / 60)} minutes` },
-                { name: 'RAM utilisée', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB` },
-            )
+                { name: 'Uptime', value: uptimeString }
+            );
 
         await interaction.reply({ embeds: [embed] });
     },
