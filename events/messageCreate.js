fad179a91f6114a280233a4f2e8b20b44408a3f6
Luna <yanngillot0@gmail.com>
2024-09-22 20:43:32 +0200

    Add files via upload

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
-        if (message.author.bot) return;
+        if (!message || !message.author || message.author.bot) return;
 
         const xpGain = Math.floor(Math.random() * 7) + 1;
         let user = await User.findOne({ userId: message.author.id });
