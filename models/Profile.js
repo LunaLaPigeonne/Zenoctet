5df45e7e36bbdbd8680bcb0eb27d3211069a2f85
LunaLaPigeonne <yann.gillot@uphf.fr>
2024-10-04 08:22:51 +0200

    Bouh

--- models/Profile.js
+++ models/Profile.js
@@ -1,12 +1,8 @@
-const mongoose = require('mongoose');
-
 const profileSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
     description: { type: String, default: 'Aucune description.' },
     passions: { type: [String], default: [] },
     favoriteGames: { type: [String], default: [] },
-    image: { type: String, default: 'default_image_url' },
+    image: { type: String, default: null }, // Pas d'URL par défaut
     clanId: { type: mongoose.Schema.Types.ObjectId, ref: 'Clan', default: null }
 });
-
-module.exports = mongoose.model('Profile', profileSchema);
